94e3269d4fec806459d0781c27d422afbe656102
Sangbaek Lee <sangbaek@jlab.org>
2019-06-18 20:16:38 -0400

    updating scripts; python scripts exexecuted by cron jobs

--- collect.py
+++ collect.py
@@ -1,11 +1,15 @@
 import subprocess
+import datetime
+
+currentDT = datetime.datetime.now()
 
 text=subprocess.Popen(['ssh','t3desk000.mit.edu','~/farmnodes.sh'], stdout=subprocess.PIPE)
 string=text.communicate()[0]
 array=string.split(' ',2)
 
 file = open(r"Sample_script_result","w")
-file.write("Total nodes: "+array[0])
+file.write("Updated on "+currentDT.strftime("%Y-%m-%d %H:%M:%S"))
+file.write("\nTotal nodes: "+array[0])
 file.write("\nBusy nodes: "+array[1])
 file.write("\nIdle nodes: "+array[2])
 file.close()
--- collect_osg.py
+++ collect_osg.py
@@ -1,11 +1,15 @@
 import subprocess
+import datetime
+
+currentDT = datetime.datetime.now()
 
 text=subprocess.Popen(['/u/home/sangbaek/osg/farmnodes.sh'], stdout=subprocess.PIPE)
 string=text.communicate()[0]
 array=string.split(' ',2)
 
 file = open(r"Sample_script_result_osg","w")
-file.write("Total nodes: "+array[0])
+file.write("Updated on "+currentDT.strftime("%Y-%m-%d %H:%M:%S"))
+file.write("\nTotal nodes: "+array[0])
 file.write("\nBusy nodes: "+array[1])
 file.write("\nIdle nodes: "+array[2])
 file.close()
