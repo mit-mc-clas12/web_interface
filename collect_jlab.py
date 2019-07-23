291c15e6971786da289e76dd2aac3635959c0808
Sangbaek <sangbaek@mit.edu>
2019-07-23 00:03:43 -0400

    skip farm name with 332

--- collect_jlab.py
+++ collect_jlab.py
@@ -9,15 +9,20 @@ b= a.communicate()[0]
 available=0
 total=0
 state='DEFAULT'
-for i, content in enumerate(re.split('684|1563|771|308|332',b)):
+c=re.split('684|1563|771|308|332',b)
+for i, array in enumerate(c):
+	content=array.split()
 	if i==0:
 		state=content[-1]
+		continue
+	if len(content)==1:
 		continue
 	job=content[1]
 	total=total+int(job.split('/')[1])
 	if state=='IDLE' or state=='MIXED':
 		available=available+int(job.split('/')[0])
 	state=content[-1]
+
 
 file = open(r"Sample_script_result_jlab","w")
 file.write("Updated on "+currentDT.strftime("%Y-%m-%d %H:%M:%S"))
