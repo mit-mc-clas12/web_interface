d09730c7eac5db87860ed1dab4622546aee399eb
Sangbaek <sangbaek@mit.edu>
2019-07-22 23:41:44 -0400

    in case a value in a column limits length

--- collect_jlab.py
+++ collect_jlab.py
@@ -1,5 +1,6 @@
 import subprocess
 import datetime
+import re
 
 currentDT = datetime.datetime.now()
 
@@ -7,15 +8,13 @@ a=subprocess.Popen("/site/scicomp/auger-slurm/bin/slurmHosts",stdout=subprocess.
 b= a.communicate()[0]
 available=0
 total=0
-for i, content in enumerate(b.split()):
-	if i<6:
-		continue
-	if i%6==2:
-		state=content
-	if i%6==4:
-		if (state=="MIXED" or state=="IDLE" or state=="ALLOCATED"):
-			available=available+int(content.split('/')[0])
-		total=total+int(content.split('/')[1])
+for i, content in enumerate(re.split('684|1563|771|308|332',b)):
+	if i>0:
+		state=content[i-1][-1]
+		job=content[i][1]
+		total=total+int(job.split('/')[1])
+		if state=='IDLE' or state=='MIXED':
+			available=available+int(job.split('/')[0])
 
 file = open(r"Sample_script_result_jlab","w")
 file.write("Updated on "+currentDT.strftime("%Y-%m-%d %H:%M:%S"))
