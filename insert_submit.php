360be8fbbf87a848176b53a5d02c8fd69e3bbe4a
Sangbaek <sangbaek@mit.edu>
2019-07-30 03:17:38 -0400

    type1 now saves scard as plain txt

--- insert_submit.php
+++ insert_submit.php
@@ -1,46 +1,35 @@
     <?php
+    $project = $_POST['project'];
     $rungroup = $_POST['rungroup'];
     $farm = $_POST['farm'];
-    $nevents = $_POST['nevents'];
     $generator = $_POST['generator'];
-    $jobs = $_POST['jobs'];
+    $gcards = $_POST['gcards'];
+    $nevents = $_POST['nevents'];
+    $genOptions = $_POST['genOptions'];
     $lumi = $_POST['lumi'];
     $tcurrent = $_POST['tcurrent'];
     $pcurrent = $_POST['pcurrent'];
     $cores = $_POST['cores'];
     $ram = $_POST['ram'];
-    if (!empty($rungroup) || !empty($farm) || !empty($nevents) || !empty($generator) || !empty($jobs) || !empty($lumi) || !empty($tcurrent) || !empty($pcurrent) || !empty($cores) || !empty($ram)) {
-        $host = "localhost";
-        $dbUsername = "root";
-        $dbPassword = "";
-        $dbname = "test";
-        //create connection
-        $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
-        if (mysqli_connect_error()) {
-         die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
-        } else {
-         $SELECT = "SELECT farm From submit Where farm = ? Limit 1";
-         $INSERT = "INSERT Into submit (rungroup, farm, nevents, generator, jobs, lumi, tcurrent, pcurrent, cores, ram) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-         //Prepare statement
-         $stmt = $conn->prepare($SELECT);
-         $stmt->bind_param("s", $farm);
-         $stmt->execute();
-         $stmt->bind_result($farm);
-         $stmt->store_result();
-         $rnum = $stmt->num_rows;
-         if ($rnum==0) {
-          $stmt->close();
-          $stmt = $conn->prepare($INSERT);
-          $stmt->bind_param("ssisiiiiii", $rungroup, $farm, $nevents, $generator, $jobs, $lumi, $tcurrent, $pcurrent, $cores, $ram);
-          $stmt->execute();
-          echo "New record inserted sucessfully";
-         } else {
-          echo "Someone already registered using this generator";
-         }
-         $stmt->close();
-         $conn->close();
+    $jobs = $_POST['jobs'];
+    if (!empty($genOptions) ||!empty($project) || !empty($gcards) || !empty($rungroup) || !empty($farm) || !empty($nevents) || !empty($generator) || !empty($jobs) || !empty($lumi) || !empty($tcurrent) || !empty($pcurrent) || !empty($cores) || !empty($ram)) {
+            $fp = fopen('scard_type1.txt', 'w');
+            fwrite($fp, 'project: '.$project.PHP_EOL);
+            fwrite($fp, 'group: '.$rungroup.PHP_EOL);
+            fwrite($fp, 'farm_name: '.$farm.PHP_EOL);
+            fwrite($fp, 'generator: '.$generator.PHP_EOL);
+            fwrite($fp, 'gcards: '.$gcards.PHP_EOL);
+            fwrite($fp, 'nevents: '.$nevents.PHP_EOL);
+            fwrite($fp, 'genOptions: '.$genOptions.PHP_EOL);
+            fwrite($fp, 'luminosity: '.$lumi.PHP_EOL);
+            fwrite($fp, 'tcurrent: '.$tcurrent.PHP_EOL);
+            fwrite($fp, 'pcurrent: '.$pcurrent.PHP_EOL);
+            fwrite($fp, 'cores_req: '.$cores.PHP_EOL);
+            fwrite($fp, 'mem_req'.$ram.PHP_EOL);
+            fwrite($fp, 'jobs: '.$jobs.PHP_EOL);
+            fclose($fp);
         }
-    } else {
+    else {
      echo "All field are required";
      die();
     }
