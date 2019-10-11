8a5c4ad4e11012fdae02d8fe9154b6bb2e619368
Sangbaek <sangbaek@mit.edu>
2019-10-11 00:09:54 -0400

    now makes image with username

--- username.php
+++ username.php
@@ -1,4 +1,14 @@
 <?php
-    $username = $_SERVER['PHP_AUTH_USER'];
-    echo($username);
+	header( "Content-type: image/png" );
+
+	$uname = imagecreatetruecolor( 300, 60 );
+	$background = imagecolorallocate( $uname, 255, 255, 255 );
+	$text_colour = imagecolorallocate( $uname, 0, 0, 0 );
+	imagefilledrectangle($uname, 0, 0, 400, 100, $background);
+	$font = './Helvetica.ttf';
+	$text=$_SERVER['PHP_AUTH_USER'];
+	imagettftext( $uname,40,0,10,45,$text_colour, $font,$text );
+
+	imagepng( $uname );
+	imagedestroy( $uname );
 ?>
