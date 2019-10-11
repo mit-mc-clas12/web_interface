bb120ecf8c7134ca5482f8cf6ea00535d38e1669
Sangbaek <sangbaek@mit.edu>
2019-10-11 00:17:29 -0400

    make image starting with logged in as..

--- username.php
+++ username.php
@@ -1,14 +1,14 @@
 <?php
-	header( "Content-type: image/png" );
+        header( "Content-type: image/png" );
 
-	$uname = imagecreatetruecolor( 300, 60 );
-	$background = imagecolorallocate( $uname, 255, 255, 255 );
-	$text_colour = imagecolorallocate( $uname, 0, 0, 0 );
-	imagefilledrectangle($uname, 0, 0, 400, 100, $background);
-	$font = './Helvetica.ttf';
-	$text=$_SERVER['PHP_AUTH_USER'];
-	imagettftext( $uname,40,0,10,45,$text_colour, $font,$text );
+        $uname = imagecreatetruecolor( 650, 60 );
+        $background = imagecolorallocate( $uname, 255, 255, 255 );
+        $text_colour = imagecolorallocate( $uname, 0, 0, 0 );
+        imagefilledrectangle($uname, 0, 0, 650, 100, $background);
+        $font = './Helvetica.ttf';
+        $text='Logged in as '.$_SERVER['PHP_AUTH_USER'];
+        imagettftext( $uname,40,0,10,45,$text_colour, $font,$text );
 
-	imagepng( $uname );
-	imagedestroy( $uname );
+        imagepng( $uname );
+        imagedestroy( $uname );
 ?>
