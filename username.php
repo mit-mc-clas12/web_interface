6e81a2f28265f0b534f7604ebdc35aaf756377fb
Sangbaek <sangbaek@mit.edu>
2019-10-12 02:46:21 -0400

    use server's default font

--- username.php
+++ username.php
@@ -1,11 +1,11 @@
 <?php
         header( "Content-type: image/png" );
 
-        $uname = imagecreatetruecolor( 650, 60 );
+        $uname = imagecreatetruecolor( 800, 60 );
         $background = imagecolorallocate( $uname, 255, 255, 255 );
         $text_colour = imagecolorallocate( $uname, 0, 0, 0 );
-        imagefilledrectangle($uname, 0, 0, 650, 100, $background);
-        $font = './Helvetica.ttf';
+        imagefilledrectangle($uname, 0, 0, 800, 100, $background);
+        $font = '/usr/share/fonts/dejavu/DejavuSans-ExtraLight.ttf';
         $text='Logged in as '.$_SERVER['PHP_AUTH_USER'];
         imagettftext( $uname,40,0,10,45,$text_colour, $font,$text );
 
