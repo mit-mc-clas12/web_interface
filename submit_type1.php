b46be76a43d92bd3da31949a35271bc91a81525f
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2019-10-16 10:22:55 -0400

    cosmetic and removing type1 default fields

--- submit_type1.php
+++ submit_type1.php
@@ -63,44 +63,33 @@
 			<header class="w3-panel w3-opacity w3-container" id="myHeader">
 				<i onclick="w3_open()" class="fa fa-bars w3-xlarge w3-button"></i>
 				<div class="w3-center">
-					<!-- <h4></h4> -->
 					<h1 class="w3-xlarge">CLAS12 Monte-Carlo Job Submission Portal</h1>
 					<br>
-				</div>
 				</div>
 			</header>
 
 			<?php
-				$project = $_POST['project'];
-				$rungroup = $_POST['rungroup'];
-				$farm = $_POST['farm'];
-				$generator = $_POST['generator'];
-				$gcards = $_POST['gcards'];
-				$nevents = $_POST['nevents'];
-				$genOptions = $_POST['genOptions'];
-				$lumi = $_POST['lumi'];
-				$tcurrent = $_POST['tcurrent'];
-				$pcurrent = $_POST['pcurrent'];
-				$cores = $_POST['cores'];
-				$ram = $_POST['ram'];
-				$jobs = $_POST['jobs'];
+				$project     = $_POST['project'];
+				$rungroup    = $_POST['rungroup'];
+				$farm        = $_POST['farm'];
+				$gcards      = $_POST['gcards'];
+				$generator   = $_POST['generator'];
+				$genOptions  = $_POST['genOptions'];
+				$nevents     = $_POST['nevents'];
+				$jobs        = $_POST['jobs'];
 				$totalevents = $_POST['totalevents'];
-				$username = $_SERVER['PHP_AUTH_USER'];
-				if (!empty($genOptions) ||!empty($project) || !empty($gcards) || !empty($rungroup) || !empty($farm) || !empty($nevents) || !empty($generator) || !empty($jobs) || !empty($lumi) || !empty($tcurrent) || !empty($pcurrent) || !empty($cores) || !empty($ram)) {
+				$username    = $_SERVER['PHP_AUTH_USER'];
+
+				if (!empty($genOptions) ||!empty($project) || !empty($rungroup) || !empty($farm) || !empty($gcards)  || !empty($generator) || !empty($nevents)  || !empty($jobs)) {
 					$fp = fopen('scard_type1.txt', 'w');
-					fwrite($fp, 'project: CLAS12  # project name'.PHP_EOL);
-					fwrite($fp, 'group: '.$rungroup.'   # project description'.PHP_EOL);
-					fwrite($fp, 'farm_name: '.$farm.' # farm pool'.PHP_EOL);
-					fwrite($fp, 'generator: '.$generator.'  # one of clasdis, dvcs, disrad. Alternatively, the online public accessible location of user lund files.'.PHP_EOL);
-					fwrite($fp, 'gcards: '.$gcards.'    # gcard, or online public accessible location of user gcards. If online address, there will be a submission for each gcard at that address'.PHP_EOL);
-					fwrite($fp, 'nevents: '.$nevents.'  # run 100 events, this include the generator'.PHP_EOL);
+					fwrite($fp, 'project: CLAS12                # project name'.PHP_EOL);
+					fwrite($fp, 'group: '.$rungroup.'           # project description'.PHP_EOL);
+					fwrite($fp, 'farm_name: '.$farm.'           # farm pool'.PHP_EOL);
+					fwrite($fp, 'gcards: '.$gcards.'            # gcard, or online public accessible location of user gcards. If online address, there will be a submission for each gcard at that address'.PHP_EOL);
 					fwrite($fp, 'genOptions: '.$genOptions.'    # clasdis options: theta between 10 and 15 degrees'.PHP_EOL);
-					fwrite($fp, 'luminosity: '.$lumi.'  # percent of 10^35 luminosity'.PHP_EOL);
-					fwrite($fp, 'tcurrent: '.$tcurrent.'    # torus field scale'.PHP_EOL);
-					fwrite($fp, 'pcurrent: '.$pcurrent.'    # solenoid field scale'.PHP_EOL);
-					fwrite($fp, 'cores_req: 1 # number of cores to request from node'.PHP_EOL);
-					fwrite($fp, 'mem_req: 2 # GB of RAM to request from node.'.PHP_EOL);
-					fwrite($fp, 'jobs: '.$jobs.'    # number of jobs for each submission. This entry is ignored if lund files are used. In that case, theres is exactly one job / file'.PHP_EOL);
+					fwrite($fp, 'generator: '.$generator.'      # one of clasdis, dvcs, disrad. Alternatively, the online public accessible location of user lund files.'.PHP_EOL);
+					fwrite($fp, 'nevents: '.$nevents.'          # run 100 events, this include the generator'.PHP_EOL);
+					fwrite($fp, 'jobs: '.$jobs.'                # number of jobs for each submission. This entry is ignored if lund files are used. In that case, theres is exactly one job / file'.PHP_EOL);
 					fclose($fp);
 					$command = escapeshellcmd('../SubMit/client/src/SubMit.py -u '.$username.' scard_type1.txt');
 					$output = shell_exec($command);
