3ef524e7404706fb9911bb63c62dbd4416b403dc
Sangbaek <sangbaek@mit.edu>
2019-10-20 20:01:22 -0400

    username php only prints username

--- username.php
+++ username.php
@@ -1,12 +1,12 @@
 <?php
         header( "Content-type: image/png" );
 
-        $uname = imagecreatetruecolor( 800, 60 );
+        $uname = imagecreatetruecolor( 400, 60 );
         $background = imagecolorallocate( $uname, 255, 255, 255 );
         $text_colour = imagecolorallocate( $uname, 0, 0, 0 );
-        imagefilledrectangle($uname, 0, 0, 800, 100, $background);
-        $font = '/usr/share/fonts/dejavu/DejavuSans-ExtraLight.ttf';
-        $text='Logged in as '.$_SERVER['PHP_AUTH_USER'];
+        imagefilledrectangle($uname, 0, 0, 400, 60, $background);
+        $font = '/usr/share/fonts/dejavu/DejaVuSans-ExtraLight.ttf';
+        $text=$_SERVER['PHP_AUTH_USER'];
         imagettftext( $uname,40,0,10,45,$text_colour, $font,$text );
 
         imagepng( $uname );
