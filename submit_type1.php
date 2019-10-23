a3078e0e8e0336deb213d1f0b3be0b469a162d3f
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2019-10-23 16:42:40 -0400

    hardcoding CLAS12 as project

--- submit_type1.php
+++ submit_type1.php
@@ -28,7 +28,7 @@
 		<div class="w3-center">
 			
 			<?php
-				$project     = $_POST['project'];
+				$project     = 'CLAS12';
 				$rungroup    = $_POST['rungroup'];
 				$farm        = $_POST['farm'];
 				$gcards      = $_POST['gcards'];
@@ -41,7 +41,7 @@
 
 				if (!empty($genOptions) ||!empty($project) || !empty($rungroup) || !empty($farm) || !empty($gcards)  || !empty($generator) || !empty($nevents)  || !empty($jobs)) {
 					$fp = fopen('scard_type1.txt', 'w');
-					fwrite($fp, 'project:    CLAS12          #'.PHP_EOL);
+					fwrite($fp, 'project:  '.$project.'      #'.PHP_EOL);
 					fwrite($fp, 'group: '.$rungroup.'        #'.PHP_EOL);
 					fwrite($fp, 'farm_name: '.$farm.'        #'.PHP_EOL);
 					fwrite($fp, 'gcards: '.$gcards.'         #'.PHP_EOL);
