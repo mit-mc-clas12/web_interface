dd11553addcf641a9d673c0a5edcd90587a9cecb
Sangbaek <sangbaek@mit.edu>
2019-11-07 17:57:42 -0500

    merge individual json files

---
+++ stats/collect.py
@@ -0,0 +1,15 @@
+import json
+from collections import OrderedDict
+
+
+str = "[\n"
+for files in ['stats_osg.json','stats_MIT.json','stats_JLab.json']:
+	with open('../stats_results/'+files,'r') as jsondata:
+		str += jsondata.readline()+",\n"
+
+str = str[:-2]
+str += "\n]"
+
+writefile = open('../stats_results/stats.json','w')
+writefile.write(str)
+writefile.close()
