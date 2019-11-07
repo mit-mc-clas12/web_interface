5de9a8603f847994e5cc9c89cf6f4b6729ae8f0f
Sangbaek <sangbaek@mit.edu>
2019-11-07 15:20:01 -0500

    no opacity at title, for clear nav bar

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 	</head>
 	<body onload='osgLogtoTable()'>
 		<!-- Header -->
-		<header class="w3-panel w3-opacity w3-container" id="myHeader">
+		<header class="w3-panel w3-container" id="myHeader">
 			
 			<ul id="nav">
 				<li><a href="index.php">Home</a></li>
@@ -23,7 +23,7 @@
 			</ul>
 
 			<div class="w3-center">
-				<h1 class="w3-xlarge">CLAS12 Monte-Carlo Job Submission Portal</h1>
+				<h1 class="w3-xlarge w3-opacity">CLAS12 Monte-Carlo Job Submission Portal</h1>
 				<h2 class="w3-xlarge" style="width:73%;text-align:right">Logged in as <?php $username= $_SERVER['PHP_AUTH_USER']; echo($username); ?> <br>
 				</h2>
 
