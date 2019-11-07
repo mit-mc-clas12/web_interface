3529dbfcb0819c4bddfd431567beb82a4b0a2f0d
Sangbaek <sangbaek@mit.edu>
2019-11-07 17:58:02 -0500

    put farm name first

--- stats/collect_osg.py
+++ stats/collect_osg.py
@@ -9,6 +9,6 @@ text=subprocess.Popen(['../stats_raw/osg/farmnodes.sh'], stdout=subprocess.PIPE)
 string=text.communicate()[0]
 array=string.split(' ',2)
 
-dic =OrderedDict([('Total cores',array[0]),('Busy cores',array[1]),('Idle cores',array[2]),('timestamp',currentDT.strftime("%Y-%m-%d %H:%M:%S")),('farm','osg')])
+dic =OrderedDict([('farm','osg'),('Total cores',array[0]),('Busy cores',array[1]),('Idle cores',array[2]),('timestamp',currentDT.strftime("%Y-%m-%d %H:%M:%S"))])
 with open('../stats_results/stats_osg.json', 'w') as outfile:
     json.dump(dic, outfile)
