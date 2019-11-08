27d99d54ce9705c49a697f224515ed4de131b42c
Sangbaek <sangbaek@mit.edu>
2019-11-07 19:11:29 -0500

    jlab script name typo
    lower cases

--- stats/collect.py
+++ stats/collect.py
@@ -3,7 +3,7 @@ from collections import OrderedDict
 
 
 str = "[\n"
-for files in ['stats_osg.json','stats_MIT.json','stats_JLab.json']:
+for files in ['stats_osg.json','stats_MIT.json','stats_jlab.json']:
 	with open('../stats_results/'+files,'r') as jsondata:
 		str += jsondata.readline()+",\n"
 
