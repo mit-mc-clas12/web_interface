008c1ca8831c60e237285e7e7979535e198f3cdc
Sangbaek <sangbaek@mit.edu>
2020-06-04 01:54:52 -0400

    update for xrootd.json to type1 scard

--- main.js
+++ main.js
@@ -22,7 +22,6 @@ function myFunction() {
 
 function genSelected(val) {
 	var generator = document.getElementById("generator").value;
-	var text = ""
 	if (generator == "clasdis") {
 		document.getElementById("generatorLink").getElementsByTagName('a')[0].href='https://github.com/JeffersonLab/clasdis-nocernlib/blob/master/README.md';
 		document.getElementById("generatorLink").getElementsByTagName('a')[0].innerHTML='clasdis options';
@@ -42,6 +41,108 @@ function genSelected(val) {
 		document.getElementById("generatorLink").getElementsByTagName('a')[0].href='https://gemc.jlab.org/gemc/html/documentation/generator/internal.html';
 		document.getElementById("generatorLink").getElementsByTagName('a')[0].innerHTML='gemc generator options';
 
+	}
+}
+
+function fieldSelected() {
+	var experiments = document.getElementById("experiments").value;
+	var text = "";
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		  if (this.readyState == 4 && this.status == 200) {
+			var myObj = JSON.parse(this.responseText);
+			var keys_field = Object.keys(myObj[experiments]);
+			for (key in keys_field){
+				text += "<option value=\""+keys_field[key]+"\">"+keys_field[key]+"</option>";
+			}
+			document.getElementById("fields").innerHTML= text;
+		}
+	};
+	xmlhttp.open("GET", "xrootd/xrootd.json", true);
+	xmlhttp.send();
+}
+
+function currentenergySelected() {
+	var experiments = document.getElementById("experiments").value;
+	var fields = document.getElementById("fields").value;
+	var text = "";
+	text += "<option selected hidden value=\"\"></option>"
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		  if (this.readyState == 4 && this.status == 200) {
+			var myObj = JSON.parse(this.responseText);
+			var vals  = myObj[experiments][fields];
+			for (val in vals){
+				text += "<option value=\""+vals[val]+"\">"+vals[val]+"</option>";
+			}
+			document.getElementById("currentenergy").innerHTML= text;
+		}
+	};
+	xmlhttp.open("GET", "xrootd/xrootd.json", true);
+	xmlhttp.send();
+}
+
+
+function bkg_rows() {
+	var e = document.getElementById("bkg_merging").value;
+	var table = document.getElementById("submission_table");
+	var row1 = table.insertRow(8);
+	var row2 = table.insertRow(9);
+	var row3 = table.insertRow(10);
+	if (e == "yes"){
+		var cell_exp = row1.insertCell(0);
+		var cell_exp_options = row1.insertCell(1);
+		var cell_field = row2.insertCell(0);
+		var cell_field_options = row2.insertCell(1);
+		var cell_currentenergy = row3.insertCell(0);
+		var cell_currentenergy_options = row3.insertCell(1);
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function() {
+		  if (this.readyState == 4 && this.status == 200) {
+			var myObj = JSON.parse(this.responseText);
+			var keys = Object.keys(myObj);
+			var txt_exp = ""
+			var keys_field;
+			cell_exp.innerHTML = "Experimental Setup";
+			txt_exp += "<select name=\"experiments\" id =\"experiments\" onchange=\"fieldSelected();\">"
+			for (key in keys){
+				txt_exp += "<option value=\""+keys[key]+"\">"
+				txt_exp += keys[key];
+				txt_exp += "</option>"
+			}
+			txt_exp += "</select>"
+			cell_exp_options.innerHTML = txt_exp;
+			cell_field.innerHTML = "Field Setting";
+			keys_field = Object.keys(myObj["rga_fall2018"])
+			var txt_field= "<select name=\"fields\" id =\"fields\">"
+			for (key in keys_field){
+				txt_field += "<option value=\""+keys_field[key]+"\">"
+				txt_field += keys_field[key];
+				txt_field += "</option>"
+			}
+			txt_field += "</select>"
+			cell_field_options.innerHTML = txt_field;
+			cell_currentenergy.innerHTML = "Current/ Energy";
+			var vals = myObj["rga_fall2018"]["tor-1.00_sol-1.00"];
+			var txt_currentenergy ="<select name=\"currentenergy\" id =\"currentenergy\"  onclick =\"currentenergySelected()\">"
+			txt_currentenergy += "<option selected hidden value=\"\"></option>"
+			for (val in vals){
+				txt_currentenergy += "<option value=\""+vals[val]+"\">"
+				txt_currentenergy += vals[val];
+				txt_currentenergy += "</option>"
+			}
+			txt_currentenergy += "</select>"
+			cell_currentenergy_options.innerHTML = txt_currentenergy;
+		  }
+		};
+		xmlhttp.open("GET", "xrootd/xrootd.json", true);
+		xmlhttp.send();
+
+	}
+	else if (e == "no"){
+		table.deleteRow(11)
+		table.deleteRow(11)
+		table.deleteRow(11)
 	}
 }
 
