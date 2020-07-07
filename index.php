fdd22f39af802db188c6759f1f57ef499fce43f2
ma <maurizio.ungaro@cnu.edu>
2020-07-07 15:25:28 -0400

    submit boxes a bit smaller

--- index.php
+++ index.php
@@ -8,24 +8,24 @@
 		<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css"/>
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway"/>
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-		<link rel="stylesheet" href="main.css">
-		<script type="text/javascript"> var username = "<?= $_SERVER['PHP_AUTH_USER'] ?>";</script>
-
-	</head>
+			<link rel="stylesheet" href="main.css">
+				<script type="text/javascript"> var username = "<?= $_SERVER['PHP_AUTH_USER'] ?>";</script>
+				
+				</head>
 	
 	<body onload='osgLogtoTable(); farmStatstoTable();'>
 		
-<!--		<h3>-->
-<!--			<center>-->
-<!--				<font color="blue">-->
-<!--	Warning: monitoring not working (working on it!). OSG Submission is working.<br/>-->
-<!--	May 4 2020 Maintanance at 3pm: <br/>-->
-<!--	the framework is being updated. <br/>-->
-<!--	Job Submission is suspended. <br/>-->
-<!--	Jobs already submitted will run normally.-->
-<!--				</font>-->
-<!--			</center>-->
-<!--		</h3>-->
+		<!--		<h3>-->
+		<!--			<center>-->
+		<!--				<font color="blue">-->
+		<!--	Warning: monitoring not working (working on it!). OSG Submission is working.<br/>-->
+		<!--	May 4 2020 Maintanance at 3pm: <br/>-->
+		<!--	the framework is being updated. <br/>-->
+		<!--	Job Submission is suspended. <br/>-->
+		<!--	Jobs already submitted will run normally.-->
+		<!--				</font>-->
+		<!--			</center>-->
+		<!--		</h3>-->
 		<header class="w3-panel w3-container" id="myHeader">
 			
 			<ul id="nav">
@@ -34,75 +34,74 @@
 				<li><a href="disk.php">      Disk Usage</a></li>
 				<li><a href="osgStats.html"> OSG Stats</a></li>
 			</ul>
-
+			
 			<div class="w3-center">
 				<h1 class="w3-xlarge w3-opacity">CLAS12 Monte-Carlo Job Submission Portal</h1>
 				<h2 class="w3-xlarge" style="width:73%;text-align:right">Logged in as <?php $username= $_SERVER['PHP_AUTH_USER']; echo($username); ?></h2>
 				<br/><br/>
-
+				
 				<div class="w3-padding w3-center">
-<!--					<div id="farmStats"></div>-->
+					<!--					<div id="farmStats"></div>-->
 					<br/><br/>
 					<div id="osgLog"></div>
 					<br/><br/>
 				</div>
 			</div>
 		</header>
-
-
+		
+		
 		<div class="w3-row-padding w3-center w3-margin-top">
+			<!--		Notice: these two must be in different lines:-->
+			<!--		<div class="w3-card w3-container" style="min-height:300px">-->
+			<!--		</div>-->
 			<a>
-				<!--		Notice: these two must be in different lines:-->
-				<!--		<div class="w3-card w3-container" style="min-height:300px">-->
-				<!--		</div>-->
 				<div class="w3-quarter">
-					<div class="w3-card w3-container" style="min-height:300px">
+					<div class="w3-card w3-container" style="min-height:220px">
+						<br/><br/>
+						<br/><br/>
 					</div>
 				</div>
 			</a>
-
+			
 			<a href="type1.html" >
 				<div class="w3-quarter">
 					<div class="w3-card w3-container" style="min-height:200px">
 						<h3>Generator<br/></h3><br/>
 						<i class="w3-margin-bottom w3-text-theme" style="font-size:120px; "></i>
 						<p style="text-align: left; font-weight: normal;">
-							- In-Container or gemc internal generator <br/>
-							- Arbitrary number of jobs <br/>
-							- Arbitrary number of events for each job (max 10,000) <br/>
+						- In-Container or gemc internal generator <br/>
+						- Arbitrary number of jobs <br/>
+						- Arbitrary number of events for each job (max 10,000) <br/>
 						</p>
 					</div>
 				</div>
 			</a>
-
+			
 			<a href="type2.html" >
 				<div class="w3-quarter">
-					<div class="w3-card w3-container" style="min-height:200px">
+					<div class="w3-card w3-container" style="min-height:220px">
 						<h3>LUND Files<br/></h3><br/>
 						<i class="w3-margin-bottom w3-text-theme" style="font-size:120px"></i>
 						<p style="text-align: left; font-weight: normal;">
-							- LUND files from a web location or directory in /volatile <br/>
-							- One job per LUND file <br/>
+						- LUND files from a web location or directory in /volatile <br/>
+						- One job per LUND file <br/>
 						</p>
 					</div>
 				</div>
 			</a>
-
+			
 			<a>
-				<!--		Notice: these two must be in different lines:-->
-				<!--		<div class="w3-card w3-container" style="min-height:300px">-->
-				<!--		</div>-->
 				<div class="w3-quarter">
-					<div class="w3-card w3-container" style="min-height:200px">
+					<div class="w3-card w3-container" style="min-height:220px">
 						<br/><br/>
 						<br/><br/>
 					</div>
 				</div>
 			</a>
-
+			
 		</div>
 	</body>
-
+	
 	<script src="main.js"></script>
-
+	
 </html>
