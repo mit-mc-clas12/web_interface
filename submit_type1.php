7e6793c1385918120894f713bc1bc72faa18627b
ma <maurizio.ungaro@cnu.edu>
2020-08-05 15:38:33 -0400

    typo on submission type

--- submit_type1.php
+++ submit_type1.php
@@ -75,9 +75,9 @@
 					fwrite($fp, 'fields: '.$fields.PHP_EOL);
 					fwrite($fp, 'bkmerging: '.$bkmerging.PHP_EOL);
 					if (strpos($uri, 'test/web_interface') !== false) {
-						fwrite($fp, 'submission: production'.PHP_EOL);
+						fwrite($fp, 'submission: devl'.PHP_EOL);
 					} else {
-						fwrite($fp, 'submission: devel'.PHP_EOL);
+						fwrite($fp, 'submission: production'.PHP_EOL);
 					}
 					fclose($fp);
 					if (strpos($uri, 'test/web_interface') !== false) {
