cc97ffdff03a375d049801bc007c25fa95141ae1
maureeungaro <maureeungaro@gmail.com>
2022-03-23 11:10:12 -0400

    generator option dynamic size

--- main.js
+++ main.js
@@ -263,3 +263,12 @@ function max_events(checkboxElem) {
 		jobs.max = "100";
 	else jobs.removeAttribute('max')
 			}
+
+function resizable (el, factor) {
+	var int = Number(factor) || 7.7;
+	function resize() {el.style.width = ((el.value.length+1) * int) + 'px'}
+	var e = 'keyup,keypress,focus,blur,change'.split(',');
+	for (var i in e) el.addEventListener(e[i],resize,false);
+	resize();
+}
+resizable(document.getElementById('genOptions'),7);
