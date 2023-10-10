44c5829f4ad820392f30bc517cfed365f4c0ab98
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2023-10-10 09:56:55 -0400

    dvcsgen re-added with new mcgen version, onepigen added

--- main.js
+++ main.js
@@ -252,7 +252,7 @@ function update_gemc_coatjava_versions() {
 
 function update_mcgen_versions() {
 
-    var default_val = "2.31b";
+    var default_val = "2.32";
     var text = "<option selected  value=\" " + default_val + "\">" + default_val + "</option>";
     var xmlhttp = new XMLHttpRequest();
 
