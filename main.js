1ed445bb8be80d61d19415c74303de8a42aa6b1e
Maurizio Ungaro <maureeungaro@gmail.com>
2026-03-04 12:27:00 -0500

    updated fairshare display summary

--- main.js
+++ main.js
@@ -646,7 +646,7 @@ function fairshareToTable() {
 
 			// Per-user summary table
 			var userSummaryTxt = "<table align=\"center\" style=\"width:60%;text-align:center\">";
-			userSummaryTxt += "<tr><th>user</th><th>total jobs</th><th>weight</th><th>pending jobs</th></tr>";
+			userSummaryTxt += "<tr><th>user</th><th>total jobs</th><th>history weight</th><th>pending jobs</th></tr>";
 			for (var i = 0; i < jobsPerUser.length; i++) {
 				userSummaryTxt += "<tr>";
 				userSummaryTxt += "<td>" + escapeHtml(jobsPerUser[i].user) + "</td>";
